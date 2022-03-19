017d36f915d02a7151514326ae3403921f95e01b
Jorge Ferrando <jorfermo@gmail.com>
2022-03-19 17:19:05 +0100

    chore: uncomment module check

--- routes/check/index.js
+++ routes/check/index.js
@@ -6,12 +6,12 @@ module.exports = async function (fastify, opts) {
   fastify.get('/:service', async function (request, reply) {
     const { service } = request.params;
     let check;
-    //try {
-    check = require('../../checks/' + service);
-    // } catch (error) {
-    //   console.log(error);
-    //   throw new Error(`Service ${service} is not available`);
-    // }
+    try {
+      check = require('../../checks/' + service);
+    } catch (error) {
+      console.log(error);
+      throw new Error(`Service ${service} is not available`);
+    }
 
     return check();
   });
